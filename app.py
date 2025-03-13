a00f9d8f0090d1e44bef74142f155bce31ef5fda
Lee <dongsoo.lee@capgemini.com>
2025-03-13 14:21:33 -0700

    alembic migration added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from db import db
 from blocklist import BLOCKLIST
 import models
@@ -24,7 +25,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "277358436549178584569138248688133963873"
@@ -87,8 +88,9 @@ def create_app(db_url=None):
             401
         )
 
-    with app.app_context():
-        db.create_all()
+    # Moved to Flask-Migrate
+    #with app.app_context():
+    #    db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
