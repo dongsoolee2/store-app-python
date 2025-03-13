c26864c02f9dbb95e118081d9d5ce66043bc4d9d
Lee <dongsoo.lee@capgemini.com>
2025-03-13 13:58:56 -0700

    jwt token added

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from db import db
+from blocklist import BLOCKLIST
 import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -28,6 +29,63 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "277358436549178584569138248688133963873"
     jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+    
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({
+                "description": "The token has been revoked.", 
+                "error": "token_revoked"
+            }),
+            401
+        )
+    
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({
+                "description": "The token is not fresh.", 
+                "error": "fresh_token_required"
+            }), 
+            401
+        )
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == str(1):
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({
+                "message": "Signature verification failed.", 
+                "error": "invalid_token"
+            }),
+            401
+        )
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({
+                "description": "Request does not contain an access token.", 
+                "error": "authrization_required"
+            }),
+            401
+        )
 
     with app.app_context():
         db.create_all()
